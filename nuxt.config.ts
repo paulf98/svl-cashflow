ae5d1f5a24719ab2aa0421af8fa95d768b5aaa1e
Paul Faass <paulfaass@gmail.com>
2023-11-02 11:32:34 +0100

    feat: add nuxtui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	modules: ['@nuxt/ui'],
+	colorMode: {
+		preference: 'light',
+	},
+	ui: {
+		global: true,
+		icons: ['heroicons'],
+	},
 	css: ['~/assets/css/main.css'],
 	postcss: {
 		plugins: {
