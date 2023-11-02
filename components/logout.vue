b11dd2081d4a37523d1b189d3777569392ac592c
Paul Faass <paulfaass@gmail.com>
2023-11-02 17:28:50 +0100

    chore: change text labels in logout section

--- components/logout.vue
+++ components/logout.vue
@@ -4,8 +4,8 @@ const session = await getSession()
 </script>
 
 <template>
-    <div class="mx-auto flex flex-col gap-4 items-center">
-        <UButton color="red" @click="signOut">Sign Out</UButton>
-        <span>Logged in as {{ session && session.user && session.user.name }}</span>
+    <div class="mx-auto flex flex-col gap-2 items-center">
+        <UButton color="red" @click="signOut">Abmelden</UButton>
+        <p class="text-sm text-gray-500 italic">Angemeldet als {{ session && session.user && session.user.name }}</p>
     </div>
 </template>
