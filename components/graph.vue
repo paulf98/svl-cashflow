b22404a9b8374f7d81670bde7773f5d0c97c4644
Paul Faass <paulfaass@gmail.com>
2023-11-07 12:17:05 +0100

    chore(graph): change labels to start dates of the period

--- components/graph.vue
+++ components/graph.vue
@@ -6,24 +6,24 @@ import { getXWeeksAgoDateSpan } from '@/utils/graph-helpers';
 const bookings = useBookingsStore()
 const { getExpensesFromDateBetween, getIncomeFromDateBetween } = storeToRefs(bookings)
 const balances = computed(() => {
-    const balances = []
+    const balances = [] as { start: Date, end: Date, balance: number }[]
     for (let i = 3; i >= 0; i--) {
         const { start, end } = getXWeeksAgoDateSpan(i)
         const expenses = getExpensesFromDateBetween.value(start, end).map((booking) => booking.amount).reduce((a, b) => a + b, 0)
         const income = getIncomeFromDateBetween.value(start, end).map((booking) => booking.amount).reduce((a, b) => a + b, 0)
         const balance = expenses + income;
-        balances.push(balance)
+        balances.push({ start, end, balance })
     }
     return balances
 })
 
 const chartData = computed(() => {
     return {
-        labels: ['4 Wochen', '3 Wochen', '2 Wochen', 'Aktuell'],
+        labels: [balances.value[0].start.toLocaleDateString(), balances.value[1].start.toLocaleDateString(), balances.value[2].start.toLocaleDateString(), balances.value[3].start.toLocaleDateString()],
         datasets: [
             {
                 label: 'Zusammenfassung der letzten Wochen',
-                data: balances.value,
+                data: balances.value.map((balance) => balance.balance),
                 fill: {
                     target: 'origin',
                     above: '#22C55E',   // Area will be green above the origin
