a83845ea0389f1a3f8a5d01d23215bcd16e1f65d
Paul Faass <paulfaass@gmail.com>
2023-11-09 21:17:25 +0100

    chore: add social preview

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <script setup lang="ts">
 useHead({
     title: 'SVL Cashflow',
+    meta: [
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { name: 'description', content: 'SVL Cashflow App - Kassenübersicht' },
+        // Social Media Preview
+        { property: 'og:image', content: './svl-cashflow.png' },
+        { property: 'og:title', content: 'SVL Cashflow' },
+        { property: 'og:description', content: 'SVL Cashflow App - Kassenübersicht' },
+        { property: 'og:url', content: 'https://svl-cashflow.paulf.dev/' },
+
+    ],
 })
 </script>
 
