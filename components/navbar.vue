dc026f1cd2aa779dbe8284c77796f444fc3c0797
Paul Faass <paulfaass@gmail.com>
2023-11-09 18:45:13 +0100

    chore: improve responsive scaling of the navbar

--- components/navbar.vue
+++ components/navbar.vue
@@ -5,11 +5,11 @@ const session = await getSession()
 
 <template>
     <nav v-if="session && session.user" class="flex flex-row justify-between items-center p-4 py-2 border-b">
-        <NuxtLink to="/" class="hover:bg-gray-200 rounded-md px-1 md:px-2">
+        <NuxtLink to="/" class="hover:bg-gray-200 rounded-md px-1 md:px-2 hidden sm:block">
             <NuxtImg src="/svl-cashflow.png" width="100" height="50" />
         </NuxtLink>
 
-        <ul class="flex flex-row items-center justify-center gap-2">
+        <ul class="flex flex-row items-center justify-center gap-1 sm:gap-2">
             <Navitem name="Dashboard" route="/" icon="i-heroicons-home" />
             <Navitem name="Buchung" route="/add-booking" icon="i-heroicons-book-open" />
             <Navitem name="Kassen" route="/cashboxes" icon="i-heroicons-banknotes" />
